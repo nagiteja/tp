86fc753e995ea74bbe3102c510088fb0b13ac484
nagiteja <sainagi9@gmail.com>
2021-04-12 19:41:06 +0800

    passes the test

--- src/test/java/command/HelpCommandTest.java
+++ src/test/java/command/HelpCommandTest.java
@@ -8,10 +8,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HelpCommandTest {
     @Test
-    public void testHelpCommand(){
+    public void testHelpCommand() {
         String temp = "====================================================" + "\nEnter 'menu' to view sample menu"
-                + "\nEnter 'reviews' to show reviews of that particular store" + "\nEnter 'add' to add a new review" +
-                "\nEnter 'home' to select a new canteen" + "\nEnter 'list' to select a new store" + "\nEnter 'login' to re-access the app as a public/admin user"
+                + "\nEnter 'reviews' to show reviews of that particular store" + "\nEnter 'add' to add a new review"
+                +
+                "\nEnter 'home' to select a new canteen" + "\nEnter 'list' to select a new store"
+                +
+                "\nEnter 'login' to "
+                +
+                "re-access the app as a public/admin user"
                 + "\nEnter 'exit' to exit the application" + "\n====================================================";
         assertEquals(temp, temp);
 
